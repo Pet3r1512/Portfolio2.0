e53e0fb8b7678b7177fc1a56a0b08eb3b2749acd
Thanh Phong <89619124+Pet3r1512@users.noreply.github.com>
2022-03-07 08:55:30 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      poppins: ["Poppins", "san-serif"],
+    },
+    colors: {
+      white: "#fff",
+      black: "#000",
+      yellow: "#ffb703",
+      orange: "#fb8500",
+      gray: "#999",
+      c: "#4895ef",
+      java: "#ae2012",
+      python: "#9bf6ff",
+    },
+  },
+  plugins: [],
+};
